e7b2ee30145821d5b77d185354d3bc7713c5795d
Adriano Gil <adrianogil.san@gmail.com>
2017-01-01 09:41:18 -0400

    Initial cube generation and rubik rotation

---
+++ Assets/Scripts/CubeGeneration.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MeshFace
+{
+    Front,
+    Back,
+    Both
+}
+
+public static class CubeGeneration  {
+
+	public static GameObject Generate(Vector3 center, Vector3 size, Material material) {
+
+        GameObject cube = new GameObject("cube");
+        GameObject cubeFace = null;
+        Vector3 direction = Vector3.zero;
+
+		direction = Vector3.up + Vector3.right;
+        cubeFace = GenerateFace(center + (-0.5f) * Vector3.Scale(size, direction), Vector3.up, Vector3.right, material, GenerateTexture(Color.red));
+        cubeFace.transform.SetParent(cube.transform);
+		cubeFace.transform.localPosition = Vector3.zero;
+
+        // direction = Vector3.up + Vector3.right
+        // cubeFace = GenerateFace(center + (0.5f) * Vector3.Scale(size, direction), direction, GenerateTexture(Color.red));
+        // cubeFace.transform.SetParent(cube.transform);
+
+        return cube;
+    }
+
+	public static GameObject GenerateFace(Vector3 position, 
+										  Vector3 direction1, 
+                                          Vector3 direction2, 
+                                          Material material, 
+                                          Texture texture) 
+    {
+		MeshBuilder meshBuilder = new MeshBuilder ();
+
+		meshBuilder.Vertices.Add (position);
+		meshBuilder.Vertices.Add (position + direction1);
+		meshBuilder.Vertices.Add (position + direction2);
+		meshBuilder.Vertices.Add (position + direction1 + direction2);
+        meshBuilder.AddQuadTriangles(MeshFace.Both, 0, 1, 2, 3);
+
+		Mesh mesh = meshBuilder.CreateMesh ();
+
+		GameObject cubeFace = new GameObject ("cubeFace");
+		MeshFilter meshFilter = cubeFace.AddComponent<MeshFilter> ();
+		MeshRenderer meshRenderer = cubeFace.AddComponent<MeshRenderer> ();
+
+		meshFilter.mesh = mesh;
+		meshRenderer.material = material;
+
+		return cubeFace;
+    }
+
+    public static void AddQuadTriangles(this MeshBuilder meshBuilder, MeshFace meshFace,
+                                        int index0, int index1, int index2, int index3)
+    {
+        if (meshFace == MeshFace.Front || meshFace == MeshFace.Both) {
+            meshBuilder.AddTriangle(index0, index1, index2);
+            meshBuilder.AddTriangle(index1, index3, index2);
+        }
+        if(meshFace == MeshFace.Back || meshFace == MeshFace.Both){
+            meshBuilder.AddTriangle(index1, index0, index2);
+            meshBuilder.AddTriangle(index1, index2, index3);
+        }
+    }
+
+    public static Texture GenerateTexture(Color color, int sizeX = 100, int sizeY = 100)
+    {
+        Texture2D texture = new Texture2D(sizeX, sizeY);
+
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        return texture;
+    }
+}
---
+++ Assets/Scripts/RubikCubeGenerator.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class RubikCubeGenerator : MonoBehaviour {
+
+	public GameObject cubePrefab;
+	public Material material;
+
+	private GameObject[,,] cubeMatrix;
+
+	public Vector3 cubePosition; 
+	public Vector3 rotationFace;
+	public float angle;
+
+	public Vector3 sumCube;
+
+	void Awake() {
+		cubeMatrix = new GameObject[3, 3, 3];
+	}
+
+	// Use this for initialization
+	void Start () {
+		// for (int x = 0; x < 3; x++) {
+		// 	for (int y = 0; y < 3; y++) {
+		// 		for (int z = 0; z < 3; z++) {
+		// 			cubeMatrix [x, y, z] = Instantiate (cubePrefab) as GameObject;
+		// 			cubeMatrix [x, y, z].transform.SetParent (transform);
+		// 			cubeMatrix [x, y, z].transform.localPosition = new Vector3 (x, y, z);
+		// 		}
+		// 	}
+		// }
+		GameObject cube = CubeGeneration.Generate(Vector3.zero, Vector3.one, material);
+		cube.transform.SetParent(transform);
+		cube.transform.localPosition = Vector3.zero;
+
+	}
+
+	void UpdateCubeMatrix()
+	{
+		Vector3 cubePosition;
+		int x = 0, y = 0, z = 0;
+		for (int i = 0; i < transform.childCount; i++) {
+			cubePosition = transform.GetChild (i).localPosition;
+
+			x = (int) cubePosition.x;
+			y = (int) cubePosition.y;
+			z = (int) cubePosition.z;
+
+			cubeMatrix[x, y, z] = transform.GetChild(i).gameObject;
+		}
+	}
+		
+	public void RotateCubes(Vector3 cubePosition, Vector3 rotationFace, float angle)
+	{
+		GameObject rotateTempObject = new GameObject ("temp");
+		rotateTempObject.transform.SetParent (transform);
+
+		Vector3 rotationSet = Vector3.one - rotationFace; // (1,1,1) - (0,1,0)
+		cubePosition.Scale(rotationFace);
+
+		int posx = 0, posy = 0, posz = 0;
+
+		sumCube = Vector3.zero;
+
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				for (int z = 0; z < 3; z++) {
+					posx = (int)(rotationSet.x * x + cubePosition.x);
+					posy = (int)(rotationSet.y * y + cubePosition.y);
+					posz = (int)(rotationSet.z * z + cubePosition.z);
+
+					sumCube += new Vector3 (posx, posy, posz);
+				}
+			}
+		}
+
+		sumCube = (1 / 27f) * sumCube;
+
+		rotateTempObject.transform.localPosition = sumCube;
+
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				for (int z = 0; z < 3; z++) {
+					posx = (int)(rotationSet.x * x + cubePosition.x);
+					posy = (int)(rotationSet.y * y + cubePosition.y);
+					posz = (int)(rotationSet.z * z + cubePosition.z);
+
+					cubeMatrix [posx, posy, posz].transform.SetParent (rotateTempObject.transform);
+				}
+			}
+		}
+
+		rotateTempObject.transform.Rotate (angle * rotationFace);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = new Color(1, 0, 0, 0.5F);
+		Gizmos.DrawCube(cubePosition+transform.position, new Vector3(1, 1, 1));
+	}
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(RubikCubeGenerator))]
+public class RubikCubeGeneratorEditor : Editor {
+
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		RubikCubeGenerator rGen = target as RubikCubeGenerator;
+
+		if (rGen == null) return;
+
+		if (GUILayout.Button("Rotate")) {
+			rGen.RotateCubes (rGen.cubePosition, rGen.rotationFace, rGen.angle);
+		}
+	}
+
+}
+#endif
